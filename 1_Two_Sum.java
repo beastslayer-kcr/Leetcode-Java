d9c419efc7d1c4070b8a5eb4fcf124c6ef22ed02
Krishna Chandra Rai <61041897+beastslayer-kcr@users.noreply.github.com>
2021-09-12 23:04:16 +0530

    Update 1_Two_Sum.java

--- 1_Two_Sum.java
+++ 1_Two_Sum.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // put in to map
+        HashMap<Integer,Integer> hp = new HashMap<>();
+        for(int i=0 ;i<nums.length;i++){
+             hp.put(nums[i],i);
+        }
+        for(int i=0 ;i<nums.length;i++){
+            
+            int diff = target - nums[i];
+            if(hp.containsKey(diff)){
+                int index = hp.get(diff);
+                if(index == i) continue;
+                return new int[]{i,index};
+            }
+        }
+        return new int[]{};
+    }
+}
